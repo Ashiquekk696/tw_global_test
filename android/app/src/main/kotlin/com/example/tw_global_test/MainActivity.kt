cc7b591e84c1fe4479005502df03bab298f78928
Ashiquekk696 <ashiquesofdevoloper@gmail.com>
2024-04-15 23:39:47 +0530

    rm

--- android/app/src/main/kotlin/com/example/tw_global_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tw_global_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/constants/endpoints.dart
+++
@@ -1,4 +0,0 @@
-class ApiEndpoints {
-  static const String baseUrl = 'https://pixabay.com/api/';
-  //static const String photos = 'photos/';
-}
